83b770852b5f066cff1a3ff94c31d1a437d99ab8
deparhan <farhan.ahmadraihan@gmail.com>
2025-02-12 22:46:37 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 // ini file Javascript
+
+console.log("Javascript is working!");
+
+function validateForm() {
+  let input = document.getElementById("main-input");
+  console.log(input.value);
+
+  if (input.value == "") {
+    alert("Tolong Isi Inputan");
+  } else {
+    /// proses konversi
+    let calc = convertToCelcius(input.value);
+
+    /// tampilkan hasil
+    document.getElementById("main-result").value = calc;
+    document.getElementById("cara-konversi").value =
+      "Diket : C =" + input.value + " F..";
+    console.log("Berhasil di Eksekusi");
+  }
+}
+/// fungsi konversi
+let convertToCelcius = (input) => {
+  return input * 2;
+};
